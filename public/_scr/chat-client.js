48af016299ad78b7a57796bbd8d8c8050f501845
Zunon <zunon@zunon.io>
2016-01-22 20:00:05 +0400

    Change chat to send cards

--- public/_scr/chat-client.js
+++ public/_scr/chat-client.js
@@ -3,11 +3,16 @@ var socket;
 socket = io();
 
 $('#chatForm').on('submit', function() {
-  socket.emit('chatMessage', $('#textfield').val());
+  var card, msg, suite;
+  suite = $('#suite').val();
+  card = $('#card').val();
+  msg = suite + card;
+  console.log(msg);
+  socket.emit('chatMessage', msg);
   $('#textfield').val('');
   return false;
 });
 
 socket.on('chatMessage', function(message) {
-  return $('#messages').append($('<li>').text(message));
+  return $('#messages').append($("<li><img src=\"/public/_img/" + message + ".png\"/></li>"));
 });
