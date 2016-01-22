1147cd2b50c152fca9be88c63078a46586c448c1
Zunon <zunon@zunon.io>
2016-01-22 18:26:06 +0400

    Workflow optimizations

--- xkout.js
+++ xkout.js
@@ -1,10 +1,18 @@
-var app, express, router;
+var app, express, http, io, port, router, socket_io;
 
 express = require('express');
 
 router = require('./scripts/router');
 
+io = require('socket.io');
+
 app = express();
+
+http = require('http').Server(app);
+
+socket_io = io(http);
+
+port = 8080;
 
 app.set('view engine', 'jade');
 
@@ -14,6 +22,10 @@ app.use('/', router);
 
 app.use('/public', express["static"]('./public'));
 
-app.listen(3000, function() {
-  console.log('Listening on 3000');
+socket_io.on('connection', function(socket) {
+  return console.log('A user connected');
+});
+
+http.listen(port, function() {
+  console.log("Listening on " + port);
 });
