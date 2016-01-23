1f1d1888566114cbe11e1f03a43d46e70b536c85
Zunon <zunon@zunon.io>
2016-01-23 16:47:05 +0400

    Completely broken /chat

--- public/_scr/chat-client.js
+++ public/_scr/chat-client.js
@@ -1,18 +1,38 @@
-var socket;
+var setSocketEvents, socket;
 
-socket = io();
+socket = '';
 
 $('#chatForm').on('submit', function() {
   var card, msg, suite;
-  suite = $('#suite').val();
-  card = $('#card').val();
-  msg = suite + card;
-  console.log(msg);
-  socket.emit('chatMessage', msg);
-  $('#textfield').val('');
+  if (socket !== '') {
+    suite = $('#suite').val();
+    card = $('#card').val();
+    msg = suite + card;
+    console.log(msg);
+    socket.emit('chatMessage', msg);
+    $('#textfield').val('');
+    setSocketEvents();
+  } else {
+    alert('Join a channel first');
+  }
   return false;
 });
 
-socket.on('chatMessage', function(message) {
-  return $('#messages').append($("<li><img src=\"/public/_img/" + message + ".png\"/></li>"));
+$('#chatConnection').on('submit', function() {
+  if ($('#user').val() !== '' && $('#room').val() !== '') {
+    console.log('Creating socket');
+    socket = io($('#room').val());
+    socket.emit('user', $('#user').val());
+  } else {
+    alert('input values');
+  }
+  return false;
 });
+
+setSocketEvents = function() {
+  socket.on('chatMessage', function(message) {
+    $('#messages').append($("<li>")).append($("<img>")).attr({
+      src: "/public/_img/" + message + ".png"
+    });
+  });
+};
