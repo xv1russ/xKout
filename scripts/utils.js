e5cc805c3d1dddceb2c715ed1a1adb71bc9a545e
student <student@RAK-TE-00XXXX-52.local>
2016-02-03 08:16:09 +0400

    Rewrite utils.js and add strict mode

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,45 +1,34 @@
-/*
- * TODO rewrite entire file
- */
-// var utils;
-//
-// utils = {};
-//
-// utils.cards = [];
-//
-// utils.genCardNames = function() {
-//   var cardNames, i, j, k, l;
-//   cardNames = {};
-//   for (i = k = 1; k <= 6; i = k += 1) {
-//     cardNames[i] = [];
-//     for (j = l = 1; l <= 9; j = l += 1) {
-//       cardNames[i][j] = utils.createCardName();
-//     }
-//   }
-//   return cardNames;
-// };
-//
-// utils.createCardName = function() {
-//   var position;
-//   position = Math.floor(Math.random() * utils.cards.length);
-//   return utils.cards.splice(position, 1);
-// };
-//
-// utils.resetCards = function() {
-//   var i, k, results;
-//   results = [];
-//   for (i = k = 1; k <= 13; i = k += 1) {
-//     utils.cards[i - 1] = "club" + i;
-//     utils.cards[i + 12] = "dmnd" + i;
-//     utils.cards[i + 25] = "hart" + i;
-//     utils.cards[i + 38] = "spad" + i;
-//     if (i < 3) {
-//       results.push(utils.cards[i + 51] = "joke" + i);
-//     } else {
-//       results.push(void 0);
-//     }
-//   }
-//   return results;
-// };
-//
-// module.exports = utils;
+'use strict'
+let utils = {}
+
+utils.cards = []
+
+utils.genCardNames = () => {
+  let cardNames = {}
+  for (let i = 1; i <= 6; i++) {
+    cardNames[i] = []
+    for (let j = 1; j <= 9; j++) {
+      cardNames[i][j] = utils.createCardName()
+    }
+  }
+  return cardNames
+}
+
+utils.createCardName = () => {
+  let position = Math.floor(Math.random() * utils.cards.length)
+  return utils.cards.splice(position, 1)
+}
+
+utils.resetCards = () => {
+  for (let i = 1; i <= 13; i++) {
+    utils.cards[i - 1] = `club${i}`
+    utils.cards[i + 12] = `dmnd${i}`
+    utils.cards[i + 25] = `hard${i}`
+    utils.cards[i + 38] = `spad${i}`
+    if (i < 3) {
+      utils.cards[i + 51] = `joke${i}`
+    }
+  }
+}
+
+module.exports = utils
