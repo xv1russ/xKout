79d2fc0f77c028df33fe88c66339f32595b1f7c5
student <student@RAK-TE-00XXXX-52.local>
2016-02-03 08:46:56 +0400

    add comments to xkout.js
    To ensure readability and to
    ease further modification

--- xkout.js
+++ xkout.js
@@ -1,52 +1,71 @@
 'use strict'
+/*
+  ~~~ DECLARATIONS ~~~
+*/
+// -- CONSTANTS --
+// Requires
 const express = require('express')
 const router = require('./scripts/router')
 const io = require('socket.io')
+// Instants
 const app = express()
 const http = require('http').Server(app)
 const socketIO = io(http)
 const chat = socketIO.of('/chat')
+// Options
 const port = 8080
-
+// -- VARIABLES --
+// Objects
 let people = {}
 let rooms = {}
-
+/*
+  ~~~ EXPRESS ~~~
+*/
+// -- SETTINGS --
+// Options
 app.set('view engine', 'jade')
 app.set('views', './views')
+// Middleware
 app.use('/', router)
 app.use('/public', express['static']('./public'))
-
+/*
+  ~~~ SOCKET.IO ~~~
+*/
+// -- ON CONNECTION --
 chat.on('connection', socket => {
+  // Initialise object of current client
   people[socket.id] = {}
-  console.log('A user connected')
-
+  // On request to join a room
   socket.on('joinRoom', roomName => {
-    console.log(people[socket.id].name + ' joined ' + roomName)
+    // Join socket to said room
     socket.join(roomName)
+    // Update objects
     people[socket.id].room = roomName
     rooms[roomName].users[socket.id] = people[socket.id]
   })
-
+  // On request to leave a room
   socket.on('leaveRoom', roomName => {
-    console.log(people[socket.id].name + ' left ' + roomName)
+    // Remove socket from said room
     socket.leave(roomName)
+    // Update objects
     people[socket.id].room = ''
     delete rooms[roomName].users[socket.id]
   })
+  // On username declaration
   socket.on('user', userName => {
+    // Update the sockets' object with said username
     people[socket.id].name = userName
   })
-
-  socket.on('disconnect', () => {
-    console.log('A user disconnected')
-  })
-
+  // On message sent
   socket.on('chatMessage', message => {
-    console.log('Recieved message: ' + message)
+    // Send the message to people in the same room
     chat.to(people[socket.id].room).emit('chatMessage', message, people[socket.id].name)
   })
 })
 
+/*
+  ~~~ START LISTENING ~~~
+*/
 http.listen(port, () => {
   console.log('Listening on ' + port)
 })
